68c7ed4e8881c7d775e632fbd8b3336fe4fe6211
philip Opiyo <opiyophilipo@gmail.com>
2024-06-28 22:27:10 +0300

    feat: Add express.json middleware for parsing JSON bodies

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require('./routes/index');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // Middleware to parse JSON bodies
 app.use('/', routes);
 
 app.listen(PORT, () => {
