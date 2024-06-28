8a1e8050f5956f3fa1562f2a8268ff7ba883db3b
philip Opiyo <opiyophilipo@gmail.com>
2024-06-28 22:26:20 +0300

    feat: Add UsersController with postNew method for creating new users

---
+++ controllers/UsersController.js
@@ -0,0 +1,36 @@
+const crypto = require('crypto');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userExists = await dbClient.db.collection('users').findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+    const createdUser = {
+      id: result.insertedId,
+      email: newUser.email,
+    };
+
+    return res.status(201).json(createdUser);
+  }
+}
+
+module.exports = UsersController;
