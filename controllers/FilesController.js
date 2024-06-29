daf48664f29061fd8d3d787fe63863a5c9a54bb7
philip Opiyo <opiyophilipo@gmail.com>
2024-06-29 21:34:23 +0300

    feat: Add getFile route to FilesController for retrieving file content

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs').promises;
 const path = require('path');
 const { ObjectId } = require('mongodb');
+const mime = require('mime-types');
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
@@ -175,6 +176,42 @@ class FilesController {
     const updatedFile = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId) });
 
     return res.status(200).json(updatedFile);
+  }
+
+  static async getFile(req, res) {
+    const fileId = req.params.id;
+    const file = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId) });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    if (!file.isPublic) {
+      const token = req.headers['x-token'];
+      if (!token) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      const key = `auth_${token}`;
+      const userId = await redisClient.get(key);
+      if (!userId || userId !== file.userId.toString()) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+    }
+
+    if (file.type === 'folder') {
+      return res.status(400).json({ error: "A folder doesn't have content" });
+    }
+
+    if (!file.localPath || !(await fs.stat(file.localPath).catch(() => false))) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const mimeType = mime.lookup(file.name);
+    const fileContent = await fs.readFile(file.localPath);
+
+    res.setHeader('Content-Type', mimeType);
+    return res.status(200).send(fileContent);
   }
 }
 
